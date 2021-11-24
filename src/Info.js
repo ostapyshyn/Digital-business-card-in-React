37bbd50b6a8fb35e8fd78edd2cf421251f2fe24b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-11-24 12:00:28 +0200

    Finish card

--- src/Info.js
+++ src/Info.js
@@ -5,15 +5,15 @@ function Info() {
         <div className="info">
             <img src="images/person.png" alt="person" className="info-image"/>
             <div className="container">
-                <h1>Laura Smith</h1>
-                <h2>Frontend Developer</h2>
-                <div>laurasmith.website</div>
+                <h1 className='name'>Laura Smith</h1>
+                <h2 className="position">Frontend Developer</h2>
+                <div className='info-site'>laurasmith.website</div>
                 <div className="buttons">
-                    <button type="button" className="button">
-                    <span className="button__icon">
-                        <ion-icon name="logo-linkedin"></ion-icon>
+                    <button type="button" className="button-email">
+                    <span className="button__icon__email">
+                        <ion-icon name="mail"></ion-icon>
                     </span>
-                        <span className="button__text">Email</span>
+                        <span className="button__text__email">Email</span>
                     </button>
 
                     <button type="button" className="button">
